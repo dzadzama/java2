40085d6fb58fa3fc194aa70072d6740dbf6b2cae
dzadzama <106168094+dzadzama@users.noreply.github.com>
2022-11-22 13:43:57 +0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,98 @@
+1.
+
+//function sum(...numbers){
+  //  let sum =0;
+   // for(let positive of numbers){
+    //    if (positive > 0){
+     //       sum += positive;
+       // }
+    //}
+   // return sum;
+//}
+
+ //let result = sum(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+ //console.log(result);
+
+ 2.
+
+ //function sum(...numbers) {
+  //let numberSum = 0;
+  //for (let items of numbers) {
+  //  numberSum += items;
+ // }
+  // return numberSum;
+ //}
+ //let results = sum(10, 50, 6, 7, 8, 11, 6, 3, 9);
+ //console.log(results);
+
+ 3.
+
+ // let user = {
+//     firstname: "giorgi",
+//     lastname: "saakadze",
+//     age: 32,
+//     isloggedin: true,
+//   };
+//   if (user.isloggedin == true) {
+//     console.log(`${user.firstname} ${user.lastname}`);
+//   } else {
+//     console.log("false");
+//   }
+
+4.
+
+ //function getMax(...numbers) {
+  //  let max = 0;
+   // for(let item of numbers) {
+    //     if(item > max) {
+     //        max = item;
+      //  }
+    // }
+    // return max;
+//}
+//let resultMax =  getMax(3, 26, 29, 45,60, 120, 150, 210);
+ //console.log(resultMax);
+
+ 5.
+
+// let array = [3, 1, 2, 16, 18, 6, 9, 65];
+//for (items of array) {
+   //if (items > 0 && items < 10) {
+   // console.log(items);
+  // }
+ //}
+
+ 6.
+
+ // let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for(let items of numbers){
+ //    if(items == 5){  break;
+  //   }
+ //    console.log(items);
+// }
+
+7.
+
+// let array = ["html", "css", "python", "javascript", "bootstrap"];
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i].length > 4 && array[i].includes("av")) {
+//     console.log(array[i]);
+//   }
+// }
+
+
+// let array = ["html", "css", "python", "javascript", "bootstrap"];
+// for(let items of array){
+//     if(items.length > 4 && items.includes('av')){
+//         console.log(items);
+//     }
+// }
+
+
+// let array = ["html", "css", "python", "javascript", "bootstrap"];
+//  array.forEach((item) =>{
+//     if(item.length > 4 && item.includes('av')){
+//         console.log(item);
+//     }
+// })
+
